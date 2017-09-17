a4150db80ec14458159b59a70c494a15bec3248b
notyy <notyycn@gmail.com>
2017-09-18 07:24:10 +0800

    simplify code

--- app/utils/JsonFormatter.scala
+++ app/utils/JsonFormatter.scala
@@ -10,10 +10,7 @@ object JsonFormatter {
   implicit val customDateWrites: Writes[Date] = dateWrites("yyyy-MM-dd HH:mm:ss")
 
   implicit object CharFormats extends Format[Char] {
-    override def writes(c: Char): JsValue = c match {
-      case 'Y' => Json.toJson("Y")
-      case 'N' => Json.toJson("N")
-    }
+    override def writes(c: Char): JsValue = Json.toJson(c.toString)
 
     override def reads(json: JsValue): JsResult[Char] = json match {
       case JsString(x) if x.length == 1 => JsSuccess(x.head)
